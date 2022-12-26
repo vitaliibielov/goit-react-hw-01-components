40b7eea88bd35b9689aa0e67599f51158d0ba952
vitaliybelov.bvs <vitaliybelov.bvs@gmail.com>
2022-12-26 17:56:24 +0200

    2 task done

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,25 @@
 import css from './Statistics.module.css';
-
+import getRandomColor from 'utils/randomColors';
 import PropTypes from 'prop-types';
+
 
 export default function Statistics({ title, stats }) {
   return (
-    <section className={css.statistics}>
+    <div className={css.containerTwo}>
+      <section className={css.statistics}>
       {/* <h2 className={css.title}>Upload stats</h2> */}
       {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(stats => (
-          <li key={stats.id} className={css.item}>
+          <li key={stats.id} className={css.item} style={ {backgroundColor: getRandomColor()}}>
             <span className={css.label}>{stats.label}</span>
             <span className={css.percent}>{stats.percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
+    </div>
   );
 }
 
---
+++ src/utils/randomColors.js
@@ -0,0 +1,5 @@
+
+export default function getRandomColor() {
+	const hex = Math.floor(Math.random() * 16777215).toString(16)
+	return `#${hex}`
+}
